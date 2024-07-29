2fc10f2f3eb3daf663dc81d223b66819e80bcec4
Petar <petyyr@gmail.com>
2024-07-29 17:50:14 +0100

    fixing the parking spot page

---
+++ src/main/java/bg/softuni/parking/model/dto/NewReservationDto.java
@@ -0,0 +1,88 @@
+package bg.softuni.parking.model.dto;
+
+import java.time.LocalDateTime;
+
+public class NewReservationDto {
+
+//    private String parkingSpotLocation;
+//    private LocalDateTime startTime;
+//    private LocalDateTime endTime;
+//    private Long vehicleId;
+//
+//    // Getters and Setters...
+//
+//    public String getParkingSpotLocation() {
+//        return parkingSpotLocation;
+//    }
+//
+//    public void setParkingSpotLocation(String parkingSpotLocation) {
+//        this.parkingSpotLocation = parkingSpotLocation;
+//    }
+//
+//    public LocalDateTime getStartTime() {
+//        return startTime;
+//    }
+//
+//    public void setStartTime(LocalDateTime startTime) {
+//        this.startTime = startTime;
+//    }
+//
+//    public LocalDateTime getEndTime() {
+//        return endTime;
+//    }
+//
+//    public void setEndTime(LocalDateTime endTime) {
+//        this.endTime = endTime;
+//    }
+//
+//    public Long getVehicleId() {
+//        return vehicleId;
+//    }
+//
+//    public void setVehicleId(Long vehicleId) {
+//        this.vehicleId = vehicleId;
+//    }
+
+
+
+    private Long parkingSpotId;
+    private LocalDateTime startTime;
+    private LocalDateTime endTime;
+    private Long vehicleId;
+
+    public Long getParkingSpotId() {
+        return parkingSpotId;
+    }
+
+    public NewReservationDto setParkingSpotId(Long parkingSpotId) {
+        this.parkingSpotId = parkingSpotId;
+        return this;
+    }
+
+    public LocalDateTime getStartTime() {
+        return startTime;
+    }
+
+    public NewReservationDto setStartTime(LocalDateTime startTime) {
+        this.startTime = startTime;
+        return this;
+    }
+
+    public LocalDateTime getEndTime() {
+        return endTime;
+    }
+
+    public NewReservationDto setEndTime(LocalDateTime endTime) {
+        this.endTime = endTime;
+        return this;
+    }
+
+    public Long getVehicleId() {
+        return vehicleId;
+    }
+
+    public NewReservationDto setVehicleId(Long vehicleId) {
+        this.vehicleId = vehicleId;
+        return this;
+    }
+}
